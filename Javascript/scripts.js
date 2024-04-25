eae6a61621c3a958276a35f88aeddd108dbb70a2
Valery Bermudez <139395288+ValeryBermudez@users.noreply.github.com>
2024-04-25 14:08:08 -0500

    Update scripts.js

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -231,5 +231,8 @@ fetch('http://localhost:3000/products')
         option.textContent = addition.name + ' - $' + addition.price.toFixed(2);
         additionsSelect.appendChild(option);
         });
-
+}
+function scrollToIndex() {
+    window.location.href = 'index.html';
+}
 }
